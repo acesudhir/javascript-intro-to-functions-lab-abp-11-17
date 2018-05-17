c3de7c66c4024411cf47e093262712c912b75a9d
Sudhir Menon <sudhirhpp@gmail.com>
2018-05-17 14:08:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function shout (sString){
+function shout(sString){
   return (sString.toUpperCase());
 }
 
-function whisper (sString){
+function whisper(sString){
   return (sString.toLowerCase());
 }
 
-function logShout (sString){
+function logShout(sString){
   return (console.log(sString.toUpperCase()));
+}
+
+function logWhisper(sString){
+  return (console.log(sString.toLowerCase()));
 }
